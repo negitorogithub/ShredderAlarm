eadf9ba37c039ccdaac563457500f5ff3781ca40
negitorogithub <unifarproject35@gmail.com>
2019-03-16 19:47:11 +0900

    omise -> stripe

---
+++ app/src/main/java/unifar/unifar/shredderalarm/StripeFragment.kt
@@ -0,0 +1,129 @@
+package unifar.unifar.shredderalarm
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.stripe.android.view.CardInputWidget
+import com.stripe.android.Stripe
+import com.stripe.android.TokenCallback
+import com.stripe.android.model.Token
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [StripeFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [StripeFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class StripeFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+    private var listener: OnFragmentInteractionListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_stripe, container, false)
+        val cardInputWidget = view.findViewById(R.id.card_input_widget) as CardInputWidget
+        val stripe = activity?.let { Stripe(it, "pk_test_Ur1C5UHloH1S7kDHsDkALkvE") }
+        cardInputWidget.card?.let {
+            stripe?.createToken(
+                it,
+                object : TokenCallback {
+                    override fun onSuccess(token: Token) {
+                        // Send token to your server
+                    }
+
+                    override fun onError(error: Exception) {
+                        // Show localized error message
+                        Toast.makeText(
+                            context,
+                            error.localizedMessage,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            )
+        }
+        return view
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    fun onButtonPressed(uri: Uri) {
+        listener?.onFragmentInteraction(uri)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnFragmentInteractionListener) {
+            listener = context
+        } else {
+            throw RuntimeException("$context must implement OnFragmentInteractionListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        listener = null
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     *
+     *
+     * See the Android Training lesson [Communicating with Other Fragments]
+     * (http://developer.android.com/training/basics/fragments/communicating.html)
+     * for more information.
+     */
+    interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        fun onFragmentInteraction(uri: Uri)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment StripeFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1 : String = "default", param2: String = "default") =
+            StripeFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
